808790c7cf6491206d8aec102e6de7e332f302f3
FiYx55 <120048999+FiYx55@users.noreply.github.com>
2022-12-07 19:49:09 +0100

    Update bash_script.bash

--- bash_script.bash
+++ bash_script.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
-mkdir Desktop Documents Downloads Pictures Videos Music
+mkdir Desktop 
+mkdir Documents 
+mkdir Downloads 
+mkdir Pictures 
+mkdir Videos
 for i in 1 2 3 4 5
 do 
 mkdir folder$i
